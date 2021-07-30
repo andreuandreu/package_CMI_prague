a725dbcbdebd07f06ebc50a695289f21e6a2f258
“andreuandreugit config --global user.email “admin@linuxtechlab.com”git config --global user.name “andreuandreu <andreuaprats@gmail.com>
2021-07-30 10:08:51 +0200

    git commit in Ubuntu machine, redoing of one of the notebooks and addition of several functions in a julia script

--- src/.ipynb_checkpoints/basicent-checkpoint.jl
+++ src/.ipynb_checkpoints/basicent-checkpoint.jl
@@ -34,13 +34,13 @@ nearest neighbours aproach to estimate provabilitues, it is not able to process 
 
 """
 
-function tranfserentropy(joint, est::Kraskov,  base =2)
-    H3 = Entropies.genentropy(joint, est)
-    H2a =Entropies.genentropy(joint[:,[3,2]], est)
-    H2b=Entropies.genentropy(joint[:,[1,2]], est)
-    H1=Entropies.genentropy(joint[:,[2]], est)
-    return -H3+H2a+H2b-H1
-end
+#function tranfserentropy(joint, est::Kraskov,  base =2)
+#    H3 = Entropies.genentropy(joint, est)
+#    H2a =Entropies.genentropy(joint[:,[3,2]], est)
+#    H2b=Entropies.genentropy(joint[:,[1,2]], est)
+#    H1=Entropies.genentropy(joint[:,[2]], est)
+#    return -H3+H2a+H2b-H1
+#end
 
 """
 tranfserentropy(joint, est::Kraskov,  α =1.0, base =2)
@@ -48,7 +48,7 @@ tranfserentropy(joint, est::Kraskov,  α =1.0, base =2)
 Estimate transfer (generalized orther α =1) for a joint contruct using a default Kraskov
 nearest neighbours aproach for a embedded dimensionality 5 
 it is not able to process α != 1 it is not able to process α != 1
-"""
+
 
 function tranfserentropy(joint, est::Kraskov, dim::Int = 5, base =2)
     print("kra5")
@@ -59,7 +59,7 @@ function tranfserentropy(joint, est::Kraskov, dim::Int = 5, base =2)
     return -H3+H2a+H2b-H1
 end
 
-
+"""
 
 
 
@@ -69,7 +69,7 @@ tranfserentropy(joint, est::KozachenkoLeonenko,  α =1.0, base =2)
 Estimate transfer (generalized orther α =1) for a joint contruct using a KozachenkoLeonenko 
 NearestNeighborEntropyEstimator aproach to estimate provabilitues, it is not able to process α != 1
 
-"""
+
 
 
 
@@ -80,7 +80,7 @@ function tranfserentropy(joint, est::KozachenkoLeonenko,  base =2)
     H1 = Entropies.genentropy(joint[:,[2]], est)
     return -H3+H2a+H2b-H1
 end
-
+"""
 
 """
 tranfserentropy(joint, est::KozachenkoLeonenko,  α =1.0, base =2)
@@ -88,7 +88,7 @@ tranfserentropy(joint, est::KozachenkoLeonenko,  α =1.0, base =2)
 Estimate transfer (generalized orther α =1) for a joint contruct using a KozachenkoLeonenko 
 NearestNeighborEntropyEstimator aproach for a embedded dimensionality 5 it is not able to process α != 1
 
-"""
+
 
 
 
@@ -102,7 +102,7 @@ function tranfserentropy(joint, est::KozachenkoLeonenko, embdim = 5, base =2)
     return -H3+H2a+H2b-H1
 end
 
-
+"""
 
 """
 tranfserentropy(joint, est::VisitationFrequency; embdim::5, α =1.0, base =2)
